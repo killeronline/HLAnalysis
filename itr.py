4a773e5f9bc736dc5c73ccf49a65bc9530f3645a
Sathish Phani Kurella <sathishphanikurella@gmail.com>
2019-11-14 19:27:52 +0530

    Create itr.py

---
+++ itr.py
@@ -0,0 +1,36 @@
+'''
+Section 17       : Gross Salary
+Section 10 (13A) : House Rent Allowance
+Section 16 (iA)  : Standard Deduction
+
+
+
+'''
+Lakh = 100000
+
+Thousand = 1000
+
+Gross_Salary = 16 *Lakh
+
+HRA = 1 *Lakh
+
+Allowances_Excempted_Us10 = HRA # + Other Exempts Us10
+
+Net_Salary = Gross_Salary - Allowances_Excempted_Us10
+
+Standard_Deduction_Us16 = 50 *Thousand
+
+Deductions_Us16 = Standard_Deduction_Us16 # + Other Deductions Us16
+
+Income_Under_Head_Salaries = Net_Salary - Deductions_Us16
+
+
+
+
+
+
+
+
+
+
+
